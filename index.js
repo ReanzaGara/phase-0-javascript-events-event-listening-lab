c262afe1357d5c92e6f4dea4d0f07b720e1a3fe0
+ <+>
2024-06-30 00:19:38 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const button = document.getElementById('button'); // Updated id to 'button'
+    button.addEventListener('click', function() {
+        console.log('Button clicked!');
+    })
 }
